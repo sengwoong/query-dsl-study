2cf153d2f033eacc6b17a261cbbcc5fb45b1f937
sengwoong <rje287573@gmail.com>
2024-02-28 13:22:11 +0900

    first commit

---
+++ src/main/java/com/ExamQueyDsl/DongoShop/DongoShopApplication.java
@@ -0,0 +1,13 @@
+package com.ExamQueyDsl.DongoShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DongoShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DongoShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ExamQueyDsl/DongoShop/DongoShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ExamQueyDsl.DongoShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DongoShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
